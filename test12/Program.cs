f175f78f810afd8e0dbd74586c1614c696c30f74
skaund <63222456+skaund@users.noreply.github.com>
2021-01-26 11:56:40 +0400

    Добавьте файлы проекта.

---
+++ test12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            int b = a % 2;
+
+            if (b == 0)
+            {
+                Console.WriteLine("Вы ввели четное число!");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не четное число!");
+            }
+        }
+    }
+}
